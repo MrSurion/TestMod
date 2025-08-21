a2370d5ea42cd066ce66dcc79d20a1cad1b238f7
MrSurion <de.gregorio.nicola95@gmail.com>
2025-08-21 11:00:18 +0200

    Added one Custom Block

--- src/main/java/net/surion/testmod/block/ModBlocks.java
+++ src/main/java/net/surion/testmod/block/ModBlocks.java
@@ -14,6 +14,7 @@ import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.surion.testmod.TestMod;
+import net.surion.testmod.block.custom.MagicBlock;
 
 public class ModBlocks {
 
@@ -43,6 +44,12 @@ public class ModBlocks {
                             .strength(4f)
                             .requiresTool()
                             .sounds(BlockSoundGroup.DEEPSLATE)));
+
+    public static final Block MAGIC_BLOCK = registerBlock("magic_block",
+            new MagicBlock(AbstractBlock.Settings.create()
+                    .strength(1)
+                    .requiresTool()
+                    .sounds(BlockSoundGroup.HONEY)));
 
 
 
--- src/main/java/net/surion/testmod/block/custom/MagicBlock.java
+++ src/main/java/net/surion/testmod/block/custom/MagicBlock.java
@@ -1,4 +1,48 @@
 package net.surion.testmod.block.custom;
 
-public class MagicBlock {
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.item.MaceItem;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.surion.testmod.item.ModItems;
+
+public class MagicBlock extends Block {
+
+    public MagicBlock(Settings settings) {
+        super(settings);
+    }
+
+
+    @Override
+    protected ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
+
+
+        world.playSound(player,pos, SoundEvents.BLOCK_AMETHYST_BLOCK_CHIME, SoundCategory.BLOCKS, 1f, 1f);
+        return ActionResult.SUCCESS;
+    }
+
+    @Override
+    public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
+        if(entity instanceof ItemEntity itemEntity) {
+            if(itemEntity.getStack().getItem()== ModItems.PINK_GARNET){
+                itemEntity.setStack(new ItemStack(Items.DIAMOND, itemEntity.getStack().getCount()));
+                world.playSound(entity,pos,SoundEvents.BLOCK_AMETHYST_BLOCK_CHIME,SoundCategory.BLOCKS,1f,1f);
+            }
+        }
+
+
+
+        super.onSteppedOn(world, pos, state, entity);
+    }
 }
